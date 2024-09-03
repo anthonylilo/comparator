e5b487e1854cd584ca7a04f0af08a1b53aea35fc
blooren <blooren@outlook.com>
2024-09-03 03:44:16 -0400

    modified navar dropdown values to match specific projects

--- src/components/navbar/homeNavbar.jsx
+++ src/components/navbar/homeNavbar.jsx
@@ -19,9 +19,9 @@ function HomeNavbar() {
             <Nav.Item>
               <Form.Select aria-label="Default select example">
                 <option>Select the project</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option value="1">Purina</option>
+                <option value="2">Unifier</option>
+                <option value="3">Nestlé Professional</option>
               </Form.Select>
             </Nav.Item>
             <Nav.Item>
