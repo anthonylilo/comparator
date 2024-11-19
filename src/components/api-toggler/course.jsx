3172bb9578070a94dd198d151274ec0cc5beae84
Daniel Moreno <blooren@outlook.com>
2024-11-19 17:40:18 -0300

    created resuable module for api button and title

---
+++ src/components/api-toggler/course.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { Row, Col, Container, Button } from "react-bootstrap";
+
+function ApiGuide() {
+    <Row>
+      <Col md={12}>
+        <h2>Enable the api</h2>
+      </Col>
+      <Col md={12}>
+        <Button href="https://cors-anywhere.herokuapp.com/">Link</Button>
+      </Col>
+      </Row>
+}
+export default ApiGuide;
--- src/pages/nutrition.jsx
+++ src/pages/nutrition.jsx
@@ -5,6 +5,7 @@ import Editor from "../modules/editor/editor";
 import HomeNavbar from "../components/navbar/homeNavbar";
 import VerticalButtons from "../components/navigatonButtons/verticalButtons";
 import SubNavbar from "../components/navbar/subNavbar";
+import ApiGuide from "../components/api-toggler/course.jsx";
 
 export default function Nutrition(className = "Nutrition") {
   useEffect(() => {
@@ -26,6 +27,7 @@ export default function Nutrition(className = "Nutrition") {
           <Container className="main" fluid>
             <Row className="text-center pt-4 pb-4">
               <Col md={12}>
+              <ApiGuide />
                 <h3>Select your output format:</h3>
                 <Container fluid="md">
                   <Form.Select
