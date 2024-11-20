de678f2790a102fbb559bacc88fbe6f712ab05ed
blooren <blooren@outlook.com>
2024-11-20 15:08:45 -0300

    fixed component

--- src/components/api-toggler/course.jsx
+++ src/components/api-toggler/course.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 import { Row, Col, Container, Button } from "react-bootstrap";
-
-function ApiGuide() {
+export default function AboutPage(){
+  return(
     <Row>
-      <Col md={12}>
-        <h2>Enable the api</h2>
+      <Col  md={12}>
+        <h2 className="text-center">Enable the api</h2>
       </Col>
       <Col md={12}>
-        <Button href="https://cors-anywhere.herokuapp.com/">Link</Button>
+        <Button href="https://cors-anywhere.herokuapp.com/">Enable the API</Button>
       </Col>
-      </Row>
+    </Row>
+  )
 }
-export default ApiGuide;
