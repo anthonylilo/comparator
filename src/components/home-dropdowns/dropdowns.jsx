4e1e5977566a8474235bb1f70acea66293488c2c
blooren <blooren@outlook.com>
2024-11-21 23:15:14 -0300

    changed button order

--- src/components/home-dropdowns/dropdowns.jsx
+++ src/components/home-dropdowns/dropdowns.jsx
@@ -5,26 +5,23 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 function SplitBasicExample() {
     return (
-        <Container className='p-5'>
+        <Container className='p-5 text-left'>
         <Row>
             <Col md={12}>
             <h3 className="tools-header">Tools:</h3>
             </Col>
             <Col md={12}>
                 <Dropdown as={ButtonGroup}>
-                    <Button className='section-btn' >Matrix Mirror</Button>
+                    <Button className='section-btn left-text' >Matrix Mirror</Button>
 
                     <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
 
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Purina</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Baby and Me</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Nestlé Professional</Dropdown.Item>
+                        <Dropdown.Item href="/comparator/purina">Purina</Dropdown.Item>
+                        <Dropdown.Item href="/comparator/nutrition">Baby and Me</Dropdown.Item>
+                        <Dropdown.Item href="/comparator/professional">Nestlé Professional</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
-            </Col>
-            <Col md={12}>
-                    <Button className='section-btn' >URL Status Checker</Button>
             </Col>
             <Col md={12}>
                 <Dropdown as={ButtonGroup}>
@@ -38,6 +35,9 @@ function SplitBasicExample() {
                         <Dropdown.Item href="#/action-3">Nestlé Professional</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
+            </Col>
+            <Col md={12}>
+                    <Button className='section-btn' >URL Status Checker</Button>
             </Col>
         </Row>
         </Container>
