56e0d438be2d8a04f02fd7b0f2d93a067fa7ae7b
Anthony Alvarez <anthony.alvarez@py.nestle.com>
2024-11-25 17:39:23 -0300

    MetadaPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Purina from './pages/purina';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/styles.css';
-import Nutrition from './pages/nutrition';
-import Professional from './pages/nestlePro';
-import HttpChecker from './pages/httpChecker';
-import MetaData from './pages/metaData';
-import Redirection from './pages/redirection';
-import AboutPage from './pages/about'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Purina from "./pages/purina";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/styles.css";
+import Nutrition from "./pages/nutrition";
+import Professional from "./pages/nestlePro";
+import HttpChecker from "./pages/httpChecker";
+import MetaDataPage from "./pages/metaData";
+import Redirection from "./pages/redirection";
+import AboutPage from "./pages/about";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
@@ -20,9 +20,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/comparator/nutrition" element={<Nutrition />} />
         <Route path="/comparator/professional" element={<Professional />} />
         <Route path="/comparator/http-checker" element={<HttpChecker />} />
-        <Route path="/comparator/meta-data-checker" element={<MetaData />} />
-        <Route path="/comparator/redirection-validation" element={<Redirection />} />
+        <Route
+          path="/comparator/meta-data-checker"
+          element={<MetaDataPage />}
+        />
+        <Route
+          path="/comparator/redirection-validation"
+          element={<Redirection />}
+        />
       </Routes>
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
